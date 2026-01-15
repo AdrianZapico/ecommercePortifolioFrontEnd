9646c3a160022068fad60c3a9392f5a8e5dc4077
AdrianZapico <joeytafolla123@gmail.com>
2026-01-15 18:04:51 -0300

    store change

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
-import authSliceReducer from './slices/authSlice';
 import cartSliceReducer from './slices/cartSlice';
+import authSliceReducer from './slices/authSlice'; // Se tiver auth, mantenha
 
 const store = configureStore({
     reducer: {
-        // 1. AQUI: Conectamos a API no Redux
+        // 1. AQUI: Conecta a API no Redux
         [apiSlice.reducerPath]: apiSlice.reducer,
 
-        // Seus outros reducers (Login, Carrinho)
-        auth: authSliceReducer,
+        // Seus outros reducers
         cart: cartSliceReducer,
+        auth: authSliceReducer,
     },
-    // 2. AQUI: O Middleware é OBRIGATÓRIO para o useGetProductsQuery funcionar
+    // 2. AQUI ESTÁ O SEGREDO QUE FALTA:
+    // Sem isso, o isLoading nunca funciona e o data vem vazio
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
