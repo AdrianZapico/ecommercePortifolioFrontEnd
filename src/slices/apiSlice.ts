d0439a6dd0e8590277361fa4315fe3776d224ec0
AdrianZapico <joeytafolla123@gmail.com>
2026-01-15 17:11:03 -0300

    Force API URL in apiSlice

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://ecommerceportifolio.onrender.com' });
+
 
 export const apiSlice = createApi({
-    baseQuery,
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://ecommerceportifolio.onrender.com' }),
     tagTypes: ['Product', 'Order', 'User'], // Define os tipos de dados para cache
     endpoints: (builder) => ({}), // Os endpoints serão injetados pelos outros arquivos
 });
