29675c1d6f6f11cc51982b801a77734ca4246b6e
AdrianZapico <joeytafolla123@gmail.com>
2026-01-15 17:22:19 -0300

    Forcar URL correta no apiSlice v2

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-
-
+import { BASE_URL } from '../constants'; // Pode deixar o import, mas não vamos usar
 
 export const apiSlice = createApi({
+    // AQUI ESTÁ O SEGREDO: O link completo e direto ("Hardcoded")
     baseQuery: fetchBaseQuery({ baseUrl: 'https://ecommerceportifolio.onrender.com' }),
-    tagTypes: ['Product', 'Order', 'User'], // Define os tipos de dados para cache
-    endpoints: (builder) => ({}), // Os endpoints serão injetados pelos outros arquivos
+
+    tagTypes: ['Product', 'Order', 'User'],
+    endpoints: (builder) => ({}),
 });
